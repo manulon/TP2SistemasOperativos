2a43644ab3115168762cfe442d59eb36c44bebfe
manulon <mlongoe@fi.uba.ar>
2021-07-22 11:54:55 -0300

    utils y make inicial, voy a leer documentacion

---
+++ Moda/utils/utils.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+void printNames() {
+    std::cout<<"Grupo 4"<<std::endl<<std::endl;
+    std::cout<<"102425 LONGO ELIA CATALAN, MANUEL"<<std::endl;
+    std::cout<<"102914 MORE, AGUSTIN EMANUEL"<<std::endl;
+    std::cout<<"103442 LOVERA LOPEZ, DANIEL ALEJANDRO"<<std::endl;
+    std::cout<<"104112 BURMAN, FEDERICO "<<std::endl<<std::endl;
+}
