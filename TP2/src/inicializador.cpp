51273410c635f58fafbd64be5024cabbb7b6a883
Daniel Lovera <daanilo2497@gmail.com>
2021-07-25 12:45:04 -0300

    Cargo esta version que intente, no se si sirva para continuar

---
+++ TP2/src/inicializador.cpp
@@ -0,0 +1,38 @@
+#include "../Sem-sv/sv_shm.h"
+#include "../Sem-sv/sv_sem.h"
+#include "../utils/utils.h"
+#include <iostream>
+
+int main(int argc, const char *argv[]) {
+	print_names();
+
+	std::cout << "Inicializando semaforos..." << std::endl;
+	
+	sv_sem mutex {"Mutex", 1};
+	std::cout << "Mutex:" << std::endl;
+	std::cout << "\t- " << mutex << std::endl;
+	
+	sv_sem coordinador {"Coordinador", 0};
+	std::cout << "Coordinador:" << std::endl;
+	std::cout << "\t- " << coordinador << std::endl;
+
+	sv_sem influencer {"Influencer", 0};
+	std::cout << "Influencer:" << std::endl;
+	std::cout << "\t- " << influencer << std::endl;
+	
+	std::cout << "Inicializando memoria compartida..." << std::endl;
+	sv_shm mesa {"Mesa"};
+	mesa_status_t *mesa_status;
+	mesa_status = reinterpret_cast<mesa_status_t*>(mesa.map(sizeof(mesa_status_t)));
+	mesa_status->sillas_disponibles = MAX_SILLAS;
+	mesa_status->grungers_sentados = 0;
+	mesa_status->grungers_esperando = 0;
+	mesa_status->swaggers_sentados = 0;
+	mesa_status->swaggers_esperando = 0;
+
+	std::cout << "Mesa: " << std::endl;
+	std::cout << "\t- " << mesa << std::endl;
+
+	return 0;
+}
+
